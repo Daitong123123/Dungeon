f36040deff3b740da830ab132bebaba0a4cbfbc6
Daitong <105532604@qq.com>
2018-04-14 23:34:19 +0800

    1231231231231231

---
+++ Assets/Scripts/DtScript/BridgeUp.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeUp : MonoBehaviour
+{
+    //实现木板绕一条轴线旋转
+    private Transform transform;//定义此物体的transform
+    public GameObject ball; //引用player对象，为了获取球的位置
+    Vector3 point;
+    private bool ff = false;
+    int count = 1;
+    // Use this for initialization
+    void Start()
+    {
+        transform = this.GetComponent<Transform>();
+        point = new Vector3();
+        point.Set(1, 0, 0.5f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ff == false)
+        {
+            if (ball.GetComponent<Transform>().position.z >= 0.5 && ball.GetComponent<Transform>().position.x <=- 0.5 && ball.GetComponent<Transform>().position.x >= -1.5 && ball.GetComponent<Transform>().position.z <= 3)
+            {
+                ff = true;
+            }
+        }
+        if (ff && count <= 27)
+        {
+            transform.RotateAround(point, new Vector3(1, 0, 0),-5);
+            count++;
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/DtScript/Disappear.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear : MonoBehaviour {
+    public GameObject ball;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if(true)
+        if (ball.GetComponent<Transform>().position.z >= 0.5 && ball.GetComponent<Transform>().position.x >= -0.5 && ball.GetComponent<Transform>().position.x <= 0.5 && ball.GetComponent<Transform>().position.z <= 3)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
